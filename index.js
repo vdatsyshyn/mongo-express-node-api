a7796894b7f70776bbea58ccc34d7c6dad0b7d06
Vasyl Datsyshyn <datsyshynvasyl@gmail.com>
2018-12-23 21:04:54 +0100

    First commit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const postsRoutes = require('./routes/posts');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 
 app.get('/', (req, res) => {
